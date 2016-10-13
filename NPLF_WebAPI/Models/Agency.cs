2022b8e0a5b6a8bbaf3b32144124056dbf8807bf
gwake1@gmail.com <hwdg*9E1MnwDXf*9KZ09SrV!5otzZq50>
2016-10-13 09:05:18 -0500

    initial models

---
+++ NPLF_WebAPI/Models/Agency.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NPLF_WebAPI.Models
+{
+    public class Agency
+    {
+        public int AgencyID { get; set; }
+        public string AgencyName { get; set; }
+        public int ZipCode { get; set; }
+        public string EmailAddress { get; set; }
+        public int AreaCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ContactLastName { get; set; }
+        public string ContactFirstName { get; set; }
+        public string AltContactLastName { get; set; }
+        public string AltContactFirstName { get; set; }
+        public bool Hold { get; set; }
+
+    }
+}
---
+++ NPLF_WebAPI/Models/AgencyService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NPLF_WebAPI.Models
+{
+    public class AgencyService
+    {
+        public int AgencyServiceID { get; set; }
+        public int ZipCode { get; set; }
+        public virtual ICollection<ServiceCategory> ServiceCategoriess { get; set; }
+    }
+}
